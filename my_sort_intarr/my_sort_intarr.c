1fd6ac4474b764882db92c8219fb016e95450bc5
rayane <rayane@debian>
2021-10-19 09:17:14 -0400

    maj

--- my_sort_intarr/my_sort_intarr.c
+++ my_sort_intarr/my_sort_intarr.c
@@ -23,12 +23,3 @@ void  my_sort_intarr(int *array, int size)
 }
 
 void  my_sort_intarr(int *array, int size);
-
-int main(){
-    int arr[7] = {9, 4, 8, 2, 3, 3, 9};
-    my_sort_intarr( arr, 7);
-    for (int i = 0; i < 7; i++){
-        printf("%d\n", arr[i]);
-    }
-    return 0;
-}
