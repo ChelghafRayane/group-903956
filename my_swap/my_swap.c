ff30469b93bb4d4cf5f5f489740fcc1a14d8d321
rayane <rayane@debian>
2021-10-19 04:57:01 -0400

    maj

--- my_swap/my_swap.c
+++ my_swap/my_swap.c
@@ -1,8 +1,16 @@
-int	my_swap(int *a, int *b)
+#include <unistd.h>
+
+void my_putchar(char c);
+
+void   my_swap(int *a, int *b)
 {
   int c;
-
+{
   c = *a;
   *a = *b;
   *b = c;
+  
+}
+return ;
+
 }
