597bc2bb63a7162edacacfcb667752586d42aba0
rayane <rayane@debian>
2021-10-22 08:36:25 -0400

    maj

--- my_sort_intarr/my_sort_intarr.c
+++ my_sort_intarr/my_sort_intarr.c
@@ -4,30 +4,18 @@ void my_putchar(char c);
 
 void  my_sort_intarr(int *array, int size)
 {
-    int tmp;
-    int sorted = 1;
-    while(sorted == 1){
-        sorted = 0;
-        for (int i = 0 ; i < size ; i++){
-            if(array[i] > array[i+1]){
-                tmp = array[i];
-                array[i] = array[i+1];
-                array[i+1] = tmp;
-                sorted = 1;
-            }
-            if (array[i] == array[i+1]){
-                continue;
+   int stock = 0;
+   int k = 0;
+        for (int j = 0 ; j < size ; j++){
+                stock = array[j];
+		k = j;
+		  for (int i = j; i < size; i++) {
+            if (array[i] < stock) {
+                stock = array[i];
+                k = i;
             }
         }
+        array[k] = array[j];
+        array[j] = stock;
     }
-}
-
-void  my_sort_intarr(int *array, int size);
-{
-    int arr[7] = {9, 4, 8, 2, 3, 3, 9};
-    my_sort_intarr( arr, 7);
-    for (int i = 0; i < 7; i++){
-        printf("%d\n", arr[i]);
-    }
-    return 0;
 }
