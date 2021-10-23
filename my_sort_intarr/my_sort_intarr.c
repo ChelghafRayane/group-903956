7b5bfe83a385f8a2b7a8ad68f02dc0546486c0e8
rayane <rayane@debian>
2021-10-23 05:11:12 -0400

    maj

--- my_sort_intarr/my_sort_intarr.c
+++ my_sort_intarr/my_sort_intarr.c
@@ -1,21 +1,28 @@
 #include <unistd.h>
 #include <stdio.h>
-void my_putchar(char c);
 
-void  my_sort_intarr(int *array, int size)
+void my_swap(int *a, int *b)
 {
-   int stock = 0;
-   int k = 0;
-        for (int j = 0 ; j < size ; j++){
-                stock = array[j];
-		k = j;
-		  for (int i = j; i < size; i++) {
-            if (array[i] < stock) {
-                stock = array[i];
-                k = i;
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+void my_sort_intarr(int *array, int size)
+{
+    int finish = 0;
+    for (int i = 0; i < size; i++)
+    {
+        finish = 1;
+        for (int j = 0; j < size - 1 - i; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                my_swap(&array[j], &array[j + 1]);
+                finish = 0;
             }
         }
-        array[k] = array[j];
-        array[j] = stock;
+        if (finish)
+            break;
     }
 }
